e5649d0ea83cb0c24ed5ec37a8377c9aff62bbd6
elcasado <139217641+elcasado@users.noreply.github.com>
2023-07-11 11:26:21 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/home" exact element={<HomePage />} />
+        <Route path="/login" exact element={<HomePage />} />
         <Route path="/" exact element={<MultiPageForm />} />
       </Routes>
     </div>
